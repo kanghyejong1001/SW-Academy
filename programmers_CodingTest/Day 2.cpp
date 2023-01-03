111a5622d5fb3deb7f345ff7aa8bc75e2773a036
kanghyejong1001 <kanghyejong1001@gmail.com>
2023-01-03 10:00:00 +0900

    Day 2 - CodingTest

---
+++ programmers_CodingTest/Day 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void) {
+    int n;
+    cin >> n;
+    
+    int* fibo = 0;
+    fibo = new int[n + 1];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    
+    if(n <= 1){
+        cout << n << endl;
+        return 0;
+    }
+    
+    for(int i = 2; i <= n; i++){
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    
+    cout << fibo[n] << endl;
+    return 0;
+}
