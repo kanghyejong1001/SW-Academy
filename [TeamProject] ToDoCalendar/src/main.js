83131cac133710aa887f1e8b8cf2da0d7f3a099d
kanghyejong1001 <kanghyejong1001@gmail.com>
2023-04-03 17:18:45 +0900

    edit TodoCalendar
    teamProject

--- [TeamProject] ToDoCalendar/src/main.js
+++ [TeamProject] ToDoCalendar/src/main.js
@@ -1,13 +1,7 @@
-// import { request } from "./api.js";
-
-// const $div = document.createElement('div')
-// request(``).then(item => {
-//     $target.appendChild(() => $div.innerHTML = `${item}`)
-//     console.log(item)
-// })
-
-import App from "./App.js";
+import App from "./components/App.js";
 
 const $target = document.querySelector('.app');
 
-new App({ $target })
+new App({ 
+    $target 
+})
